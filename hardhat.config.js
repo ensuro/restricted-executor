373958a1a9bfcdc8e4825371e77a3594a2e5b2bf
GnP <g.parrondo@gmail.com>
2023-02-14 08:35:52 -0300

    add contract sizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-web3");
 require("@openzeppelin/hardhat-upgrades");
+require("hardhat-contract-sizer");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
